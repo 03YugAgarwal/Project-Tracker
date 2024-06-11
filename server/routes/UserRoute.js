dc2104b67f7056b068ea12d22e59d638038cbf3b
Yug <03yugagarwal@gmail.com>
2024-06-11 12:45:46 +0530

    feat: user api created

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -2,7 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 
-const { test } = require("../controllers/User")
-router.get('/', test)
+const { test, createUser,getUser,getUserById,getUserId } = require("../controllers/User")
+
+router.get('/', getUser)
+router.get('/:id', getUserById)
+// router.get('/test', test)
+router.post('/login', getUserId)
+router.post('/create', createUser)
 
 module.exports = router
